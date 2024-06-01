6714b601a8d22817ea1b2f9666f44c3cd77ad7c5
yshivi <yshivi825@gmail.com>
2024-06-01 10:15:27 +0530

    some chenges

---
+++ src/scripts/script.js
@@ -0,0 +1,20 @@
+// document.addEventListener('astro:page-load', () => {
+//     function openNav() {
+//         document.getElementById("mySidenav").style.width = "250px";
+//     }
+    
+//     function closeNav() {
+//         document.getElementById("mySidenav").style.width = "0";
+//     }
+    
+//     function toggleDropdown() {
+//         var dropdownMenu = document.querySelector('.dropdown-menu');
+//         if (dropdownMenu.style.display === 'block') {
+//             dropdownMenu.style.display = 'none';
+//         } else {
+//             dropdownMenu.style.display = 'block';
+//         }
+//     }
+    
+
+// });
