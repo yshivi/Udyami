aed0f74d39a2ff198ad79867d5ff64aabca70cf8
yshivi <yshivi825@gmail.com>
2024-06-05 01:35:26 +0530

    all pages responsive for mobile screen

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,24 +1,15 @@
 
-// document.addEventListener('astro:page-load', () => {
-//     var mobileMenuIcon = document.querySelector(".header-icon");
-//     var mobileMenu = document.querySelector(".mobile-menu");
+document.addEventListener('astro:page-load', () => {
+    var mobileMenuIcon = document.querySelector(".header-icon");
+    var mobileMenu = document.querySelector(".mobile-menu");
     
-//     mobileMenuIcon.addEventListener("click", () => {
-//       if (mobileMenu.style.display === "none") {
-//         mobileMenu.style.display = "block";
+    mobileMenuIcon.addEventListener("click", () => {
+      if (mobileMenu.style.display === "none") {
+        mobileMenu.style.display = "block";
         
-//       } else {
-//         mobileMenu.style.display = "none";
+      } else {
+        mobileMenu.style.display = "none";
        
-//       }
-//     });
-// });
-
-// function toggleMobileMenu() {
-//   const menu = document.getElementById('mobileMenu');
-//   if (menu.style.display === 'flex') {
-//     menu.style.display = 'none';
-//   } else {
-//     menu.style.display = 'flex';
-//   }
-// }
+      }
+    });
+});
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,20 +1,5 @@
-// document.addEventListener('astro:page-load', () => {
-//     function openNav() {
-//         document.getElementById("mySidenav").style.width = "250px";
-//     }
-    
-//     function closeNav() {
-//         document.getElementById("mySidenav").style.width = "0";
-//     }
-    
-//     function toggleDropdown() {
-//         var dropdownMenu = document.querySelector('.dropdown-menu');
-//         if (dropdownMenu.style.display === 'block') {
-//             dropdownMenu.style.display = 'none';
-//         } else {
-//             dropdownMenu.style.display = 'block';
-//         }
-//     }
-    
-
-// });
+document.getElementById("headerIcon").addEventListener("click", function() {
+    var mobileMenu = document.getElementById("mobileMenu");
+    mobileMenu.style.display === "block" ? mobileMenu.style.display = "none" : mobileMenu.style.display = "block";
+  });
+  
