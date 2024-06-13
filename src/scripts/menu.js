3befba7df42081cc2731a8073e816114d63260b3
yshivi <yshivi825@gmail.com>
2024-06-13 10:24:04 +0530

    Corrrected the menu-bar in the phone

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,19 +1,9 @@
 
-document.addEventListener('astro:page-load', () => {
-    var mobileMenuIcon = document.querySelector(".header-icon");
-    var mobileMenu = document.querySelector(".mobile-menu");
-    
-    mobileMenuIcon.addEventListener("click", () => {
-      if (mobileMenu.style.display === "none") {
-        mobileMenu.style.display = "flex";
-        
-      } else {
-        mobileMenu.style.display = "none";
-       
-      }
-    });
-
-   
-
-   
-});
+  document.getElementById('menu-icon').addEventListener('click', function() {
+    var mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.style.display === 'flex') {
+      mobileMenu.style.display = 'none';
+    } else {
+      mobileMenu.style.display = 'flex';
+    }
+  });
