4667f9db78d74d26b353f3e4c83aab3275408c6b
yshivi <yshivi825@gmail.com>
2024-06-14 22:11:12 +0530

    some changes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,9 +1,19 @@
 
-  document.getElementById('menu-icon').addEventListener('click', function() {
-    var mobileMenu = document.getElementById('mobile-menu');
-    if (mobileMenu.style.display === 'flex') {
-      mobileMenu.style.display = 'none';
-    } else {
-      mobileMenu.style.display = 'flex';
-    }
-  });
+document.addEventListener('astro:page-load', () => {
+    var mobileMenuIcon = document.querySelector(".header-icon");
+    var mobileMenu = document.querySelector(".mobile-menu");
+    
+    mobileMenuIcon.addEventListener("click", () => {
+      if (mobileMenu.style.display === "none") {
+        mobileMenu.style.display = "flex";
+        
+      } else {
+        mobileMenu.style.display = "none";
+       
+      }
+    });
+
+   
+
+   
+});
