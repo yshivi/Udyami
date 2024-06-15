82b3aee0062bdea91c887f2c73cf9a3b858d2718
yshivi <yshivi825@gmail.com>
2024-06-15 12:37:59 +0530

    The hamburger menu is correct - it opens with a single click

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,23 +1,4 @@
 
-// document.addEventListener('astro:page-load', () => {
-//     var mobileMenuIcon = document.querySelector(".header-icon");
-//     var mobileMenu = document.querySelector(".mobile-menu");
-    
-//     mobileMenuIcon.addEventListener("click", () => {
-//       if (mobileMenu.style.display === "none") {
-//         mobileMenu.style.display = "flex";
-        
-//       } else {
-//         mobileMenu.style.display = "none";
-       
-//       }
-//     });
-
-   
-
-   
-// });
-
 document.addEventListener('astro:page-load', () => {
   var mobileMenuIcon = document.querySelector(".header-icon");
   var mobileMenu = document.querySelector(".mobile-menu");
